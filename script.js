4cf09ae52a2c56e0379a32b6941ca6cc984debe9
Le Dat Em <emdatle@gmail.com>
2024-06-05 11:20:27 +0700

    up

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+// let sections = document.querySelectorAll("section");
+
+// window.onscroll = () => {
+//   sections.forEach((sec) => {
+//     let top = window.scrollY;
+//     let offset = sec.offsetTop;
+//     let height = sec.offsetHeight;
+//     if (top >= offset && top < offset + height) {
+//       sec.classList.add("show-animate");
+//     }
+//     else {
+//       sec.classList.remove("show-animate");
+//     }
+//   });
+// };
